de490e7838c7270c2f44d02e3684670ad62b548c
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2023-12-06 12:47:20 +0100

    fix: typo

--- utils/plugin.ts
+++ utils/plugin.ts
@@ -4,5 +4,5 @@ export const name = pluginPkg.strapi.name;
 export const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
 
 export const supportsContentType = (uid?: string) => {
-  return uid?.match(/^api::/) || false;// || !uid?.match(/^\w+::/) || false; // TODO: Deleting a compoment doesn't use the entityService
+  return uid?.match(/^api::/) || false;// || !uid?.match(/^\w+::/) || false; // TODO: Deleting a component doesn't use the entityService
 };
